20fe6223b1cfc4ceb8275f378ccfaedd9ae31f4d
S Gurumoorthy <139244520+GurumoorthysHub@users.noreply.github.com>
2025-12-17 10:00:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-# app.py
-def greet(name):
-    return f"Hello, {name}!"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, world!"
 
 if __name__ == "__main__":
-    print(greet("World"))
+    app.run(host="0.0.0.0", port=3000)
